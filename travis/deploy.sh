ad136fdfb6878faa0d735c550fc25e36b28535a0
mattcarrier <mcarrieruri@gmail.com>
2016-10-10 16:19:05 -0400

    Debugging

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo $GPG_KEY_NAME
+echo $GPG_PASSPHRASE
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn verify -P sign,build-extras --settings travis/settings.xml
 fi
