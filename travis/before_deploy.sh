0039122acc47913a9f18019f68b7861599390017
mattcarrier <mcarrieruri@gmail.com>
2016-10-10 17:03:49 -0400

    Remove debugging

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-echo $TRAVIS_BRANCH
-echo $TRAVIS_PULL_REQUEST
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_db86af8b5f2d_key -iv $encrypted_db86af8b5f2d_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
     gpg --fast-import travis/codesigning.asc
--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo $ENV_WITH_SPACES
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn verify -P sign,build-extras --settings travis/settings.xml
 fi
