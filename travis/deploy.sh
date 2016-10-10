c1eb98b8419adb30cb9642b11884920a4b315f67
mattcarrier <mcarrieruri@gmail.com>
2016-10-10 16:39:06 -0400

    Add environment with spaces

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-echo $GPG_KEY_NAME
-echo $GPG_PASSPHRASE
+echo $ENV_WITH_SPACES
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn verify -P sign,build-extras --settings travis/settings.xml
 fi
